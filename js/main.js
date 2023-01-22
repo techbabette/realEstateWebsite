fa98d12447e265fa558f6ec9fa3e8a3bb1b93451
techbabette <marinakrsticrf@gmail.com>
2023-01-22 17:05:03 +0100

    Form 0,1 and rebranding

--- js/main.js
+++ js/main.js
@@ -1,44 +1,42 @@
 $(window).ready(function(){
     let input = $("#landing-input");
     let filters = $("#landing-filter");
-    let search = $("#landing-search");
-    search.hide()
     filters.hide();
-    showSearchButton();
+    input.click(function(){showFilterButton()});
+    input.blur(function(){hideFilterButton()});
+    filters.click(function(event){event.preventDefault();showFilterModal()});
+    $(window).click(function(event){
+        if (event.target === $('#filterModal')[0])
+        {
+            $("#filterModal").hide('fast')
+            input.focus();
+        }
+     })
 })
 
-function showSearchButton()
+function showFilterButton()
 {
     let input = $("#landing-input");
     let filters = $("#landing-filter");
-    let search = $("#landing-search");
-    input.click(function(){
         input.animate({width : "75%"}, {
             complete: 
             function(){
-                search.show();
-                search.animate({opacity: "100%"},{complete:function()
-                    {input.animate({width : "50%"},{complete:function(){
-                        console.log("Here!");
-                        filters.show();
-                        filters.animate({opacity : "100%"});
-                    }})}})
+                filters.show();
+                filters.animate({opacity: "100%"})
                 }})
-            }
-            )
-    input.blur(function(){
-        filters.animate({opacity: "0%"}, {complete: function()
-            {
-                filters.hide();
-                input.animate({width: "75%"}, {complete: function(){
-                    search.animate({opacity: "0%"}, {complete: function(){
-                        search.hide();
-                        input.animate({width : "100%"});
-                    }})
-            }})
-        }
-    }
-    )
 }
-)
+function hideFilterButton()
+{
+    let input = $("#landing-input");
+    let filters = $("#landing-filter");
+    filters.animate({opacity: "0%"}, {complete: function(){
+        filters.hide();
+        input.animate({width : "100%"});
+    }})
+}
+
+function showFilterModal()
+{
+    let modal = $("#filterModal");
+    modal.show('fast');
 }
